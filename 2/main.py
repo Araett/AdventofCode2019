6c45962a97bcbe7a2bbd10badc0626affcbbc287
Martynas Saulius <martynas.saulius@tesonet.com>
2019-12-02 09:19:20 +0200

    2 part 1

---
+++ 2/main.py
@@ -0,0 +1,36 @@
+def read_input_and_split_by_comma():
+    with open('input', 'r') as f:
+        return f.read().split(',')
+
+
+def sum_op(opcode, pos1, pos2):
+    return opcode[pos1] + opcode[pos2]
+
+
+def mul_op(opcode, pos1, pos2):
+    return opcode[pos1] * opcode[pos2]
+
+
+def output(opcode, output_pos, data):
+    opcode[output_pos] = data
+
+
+def main():
+    opcode = [int(x) for x in read_input_and_split_by_comma()]
+    i = 0
+    opcode[1] = 12
+    opcode[2] = 2
+    while opcode[i] != 99:
+        print(opcode[i])
+        res = 0
+        if opcode[i] == 1:
+            res = sum_op(opcode, opcode[i+1], opcode[i+2])
+        elif opcode[i] == 2:
+            res = mul_op(opcode, opcode[i+1], opcode[i+2])
+        output(opcode, opcode[i+3], res)
+        i += 4
+    print(opcode[0])
+
+
+if __name__ == '__main__':
+    main()
