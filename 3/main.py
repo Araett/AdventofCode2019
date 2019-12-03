1a1d66e902d9a3b4dfbdf6e0bfcaf37426ac5367
Martynas Saulius <martynas.saulius@tesonet.com>
2019-12-03 10:23:56 +0200

    3 part 2

--- 3/main.py
+++ 3/main.py
@@ -1,9 +1,10 @@
-directions = {
+DIRECTIONS = {
     'U': (0, 1),
     'R': (1, 0),
     'D': (0, -1),
     'L': (-1, 0),
 }
+
 
 def read_input_for_wires():
     with open('input', 'r') as f:
@@ -16,7 +17,7 @@ def parse_input(wire_input):
     coordinates = [(0, 0)]
     current = coordinates[0]
     for item in wire_input:
-        d = directions[item[:1]]
+        d = DIRECTIONS[item[:1]]
         count = int(item[1:])
         new_cord = (current[0] + count * d[0], current[1] + count * d[1])
         coordinates.append(new_cord)
@@ -35,13 +36,19 @@ def generate_lines(coordinates):
         current_cord = item
     return lines
 
+
+def find_det_sign(a, b, c):
+    det = (a[0] - c[0]) * (b[1] - c[1]) - (a[1] - c[1]) * (b[0] - c[0])
+    return 1 if det > 0 else 0
+
+
 def check_collision(w1, w2):
     a, b = w1
     c, d = w2
-    det1 = 1 if (a[0] - c[0]) * (b[1] - c[1]) - (a[1] - c[1]) * (b[0] - c[0]) > 0 else 0
-    det2 = 1 if (a[0] - d[0]) * (b[1] - d[1]) - (a[1] - d[1]) * (b[0] - d[0]) > 0 else 0
-    det3 = 1 if (c[0] - a[0]) * (d[1] - a[1]) - (c[1] - a[1]) * (d[0] - a[0]) > 0 else 0
-    det4 = 1 if (c[0] - b[0]) * (d[1] - b[1]) - (c[1] - b[1]) * (d[0] - b[0]) > 0 else 0
+    det1 = find_det_sign(a, b, c)
+    det2 = find_det_sign(a, b, d)
+    det3 = find_det_sign(c, d, a)
+    det4 = find_det_sign(c, d, b)
     if det1 != det2 and det3 != det4:
         x = 0
         y = 0
@@ -53,7 +60,23 @@ def check_collision(w1, w2):
             x = c[0]
         elif c[1] - d[1] == 0:
             y = d[1]
-        return (x, y,)
+        return x, y
+
+
+def check_distance(lines1, lines2, col_coord):
+    len1 = 0
+    len2 = 0
+    for line in lines1:
+        a, b = line
+        len1 += abs(a[0] - b[0] + a[1] - b[1])
+    for line in lines2:
+        a, b = line
+        len2 += abs(a[0] - b[0] + a[1] - b[1])
+    last_point1 = lines1[-1][1]
+    last_point2 = lines2[-1][1]
+    len1 += abs(last_point1[0] - col_coord[0] + last_point1[1] - col_coord[1])
+    len2 += abs(last_point2[0] - col_coord[0] + last_point2[1] - col_coord[1])
+    return len1 + len2
 
 
 def main():
@@ -62,18 +85,20 @@ def main():
     wire2_cor = parse_input(wire2)
     wire1_lines = generate_lines(wire1_cor)
     wire2_lines = generate_lines(wire2_cor)
-    print(wire1_lines)
-    print(wire2_lines)
     collisions = set()
+    distance = []
+    i = 0
     for w1 in wire1_lines:
+        i += 1
+        j = 0
         for w2 in wire2_lines:
+            j += 1
             col_coord = check_collision(w1, w2)
             if col_coord:
                 collisions.add(col_coord)
-    distances = [abs(x) + abs(y) for x, y in collisions]
-    print(min(distances))
-    print(collisions)
-    print(distances)
+                print(w1, w2, col_coord)
+                distance.append(check_distance(wire1_lines[:i-1], wire2_lines[:j-1], col_coord))
+    print(min(distance))
 
 
 if __name__ == '__main__':
